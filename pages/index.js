a6965cba9240f7a723ec4730c0cbdd34b5b34e7d
jdimaso <gdimaso25@gmail.com>
2023-07-31 08:57:39 -0400

    redirect

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { redirect } from 'next/navigation'
 
 export default function Home() {
-redirect('https://www.payerset.com')
   return (
     <div className="container">
       <Head>
